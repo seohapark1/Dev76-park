952ec9d0017e7849e973e8c4b72734d8e945ff28
seohapark1 <sky95878273@gmail.com>
2021-03-17 11:08:50 +0900

    Add UserController [Update]

--- UserController.java
+++ UserController.java
@@ -15,4 +15,12 @@ public class UserController {
 		
 		return "user/insert";
 	}
+	
+	// 회원수정 페이지로 이동
+	@RequestMappign("/user/update.do")
+	public String update() {
+		logger.info("경로 : update");
+		
+		return "user/update";
+	}
 }
